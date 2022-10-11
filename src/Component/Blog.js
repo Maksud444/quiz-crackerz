c6aa35e85d921b5d621d7973ce012a4a3c6fe701
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-10-12 00:25:06 +0600

    aded Frequently Asked Questions

--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -3,24 +3,32 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <div className='m-14 border-4 border-sky-500 rounded-md'>
-                <h1 className='text-3xl font-bold'>Question: what is the purpose of react router dom?</h1>
-                <br />
-                <h2 className='text-2xl'>Ans: React Router DOM is an npm package that enables you to implement dynamic routing in a web app. It allows you to display pages and allow users to navigate them. It is a fully-featured client and server-side routing library for React</h2>
-            </div>
-            <br />
-            <div className='m-14 border-4 border-sky-500 rounded-md'>
-                <h1 className='text-3xl font-bold'>Question: how does react context api work?</h1>
-                <br />
-                <h2 className='text-2xl'>Ans: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</h2>
-            </div>
-            <br />
-            <div className='m-14 border-4 border-sky-500 rounded-md'>
-                <h1 className='text-3xl font-bold'>Question: what is useref in react?</h1>
-                <br />
-                <h2 className='text-2xl'>Ans: Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with , React will set its .current property to the corresponding DOM node whenever that node changes.</h2>
-            </div>
-
+            <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
+		<h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+		<div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">what is the purpose of react router dom?</summary>
+				<div className="px-4 pb-4">
+					<p>Ans: React Router DOM is an npm package that enables you to implement dynamic routing in a web app. It allows you to display pages and allow users to navigate them. It is a fully-featured client and server-side routing library for React</p>
+				</div>
+			</details>
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">Question: how does react context api work?</summary>
+				<div className="px-4 pb-4">
+					<p>Ans: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux</p>
+				</div>
+			</details>
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">Question: what is useref in react?</summary>
+				<div className="px-4 pb-4 space-y-2">
+					<p>Ans: Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with , React will set its .current property to the corresponding DOM node whenever that node changes.</p>
+				</div>
+			</details>
+		</div>
+	</div>
+</section>
+           
         </div>
     );
 };
